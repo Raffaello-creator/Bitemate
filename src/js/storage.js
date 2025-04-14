5f08ca949031eab44682a3e6b6eb57efecad5619
Artem Strelnikov <piramida1237@gmail.com>
2025-04-14 17:40:33 +0200

    task_add to cart

--- src/js/storage.js
+++ src/js/storage.js
@@ -1,1 +1,26 @@
 //Робота з loacalStorage
+
+const LS_KEY = 'basket';
+
+const addToCart = document.querySelector('.add-to-cart');
+addToCart.addEventListener('click', clickAddToCart);
+
+function clickAddToCart(event) {
+  if (!event.target.classList.contains('js-add')) {
+    return;
+  }
+  const parent = event.target.closest('.js-product');
+  const productId = Number(parent.dataset.id);
+  const currentProduct = instruments.find(({ id }) => id === productId);
+  const products = JSON.parse(localStorage.getItem(LS_KEY)) ?? [];
+
+  const index = products.findIndex(({ id }) => id === productId);
+  if (index === -1) {
+    currentProduct.qty = 1;
+    products.push(currentProduct);
+  } else {
+    products[index].qty += 1;
+  }
+
+  localStorage.setItem(LS_KEY, JSON.stringify(products));
+}
